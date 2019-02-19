970aa789cec4d287c45fa61a97f8bc5b982de8b2
guerrammax <maximino_guerra@hotmail.com>
2019-02-19 16:27:57 -0700

    changed program

--- Program.cs
+++ Program.cs
@@ -14,11 +14,15 @@ namespace StoreNet
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+ public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().Build();
+        // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseStartup<Startup>();
     }
 }
